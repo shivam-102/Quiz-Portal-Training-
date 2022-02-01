51be337a20cbc00c983554d7fc6a2ea6c2b94e9b
shivamverma_j@epam.com <shivamverma_j@epam.com>
2022-02-01 14:40:01 +0530

    Not yet done

--- Lambda_Expressions Tasks/src/main/java/com/impl/lambda/TreeMap_Desc.java
+++ Lambda_Expressions Tasks/src/main/java/com/impl/lambda/TreeMap_Desc.java
@@ -1,17 +1,24 @@
 package com.impl.lambda;
 
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class TreeMap_Desc {
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter the number of inputs:");
-        //int amount= scanner.nextInt();
         TreeMap<String,Integer> treeMap=new TreeMap<String,Integer>();
-
-
-
+        treeMap.put("Shivam",102);
+        treeMap.put("Azhar",203);
+        treeMap.put("Akhil",509);
+        treeMap.put("Deepak",20);
+        treeMap.put("Durga",199);
+        treeMap.put("Harsh",90);
+        SortedMap<String, Integer> sortedMap = new TreeMap<>(new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s2.compareTo(s1);
+            }
+        });
     }
 }
 
